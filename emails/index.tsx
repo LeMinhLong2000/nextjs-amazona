bfd981ead6a7533f5a9547c668ed73318a339ca9
Long <leminhlong11b8@gmail.com>
2025-02-25 10:03:28 +0700

    install stripe, pay order by stripe

--- emails/index.tsx
+++ emails/index.tsx
@@ -8,7 +8,8 @@ const resend = new Resend(process.env.RESEND_API_KEY as string);
 export const sendPurchaseReceipt = async ({ order }: { order: IOrder }) => {
   await resend.emails.send({
     from: `${SENDER_NAME} <${SENDER_EMAIL}>`,
-    to: (order.user as { email: string }).email,
+    // to: (order.user as { email: string }).email,
+    to: ["leminhlong11b8@gmail.com"],
     subject: "Order Confirmation",
     react: <PurchaseReceiptEmail order={order} />,
   });
