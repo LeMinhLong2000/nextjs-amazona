27c2e6904112f4b71a3b64e623072b7220ed6b04
Long <leminhlong11b8@gmail.com>
2025-02-26 11:53:32 +0700

    create admin order; mark orders as paid delivered

--- hooks/use-cart-store.ts
+++ hooks/use-cart-store.ts
@@ -86,13 +86,18 @@ const useCartStore = create(
         });
 
         // find item just add in cart; return clientId
-        // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-        return updatedCartItems.find(
+        const foundItem = updatedCartItems.find(
           (x) =>
             x.product === item.product &&
             x.color === item.color &&
             x.size === item.size
-        )?.clientId!;
+        );
+
+        if (!foundItem) {
+          throw new Error("Item not found in cart");
+        }
+
+        return foundItem.clientId;
       },
       updateItem: async (item: OrderItem, quantity: number) => {
         const { items, shippingAddress } = get().cart;
